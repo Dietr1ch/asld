1fa80f940a81886c4561d4440c8590bfd178f42a
Dietrich Daroch <Dietr1ch@acm.org>
2016-04-11 01:05:11 -0300

    Add script to run all queries

---
+++ all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Running A*"
+for i in {0..15}; do
+	timeout --kill-after 4100 3600 ./run.py -w 1 -q $i  $@  &&  sleep 120
+done
+
+echo "Running Dijstra"
+for i in {0..15} ;do
+	timeout --kill-after 4100 3600 ./run.py -w 0 -q $i  $@  &&  sleep 120
+done

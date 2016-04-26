8cd442339bab6b0d245170dd2a090fa9489b0310
Dietrich Daroch <dietr1ch@acm.org>
2016-04-26 13:36:01 -0300

    Add batch compare script

--- all.sh
+++ all.sh
@@ -13,7 +13,7 @@ for b in 1 5 10 20 40 80; do
 
 	echo "Running A* b=$b"
 	for i in $queries; do
-	#for i in 4 8 9 10 12 14 15; do
+	#for i in 4 8 9 10 12 14 15 16 17 18; do
 		timeout --kill-after 4100 3600 ./run.py -w 1 -q $i --pool-size $b --time 3000  $@
 		echo "Waiting a bit..."
 		sleep 30
@@ -21,7 +21,7 @@ for b in 1 5 10 20 40 80; do
 
 	echo "Running Dijstra b=$b"
 	for i in $queries; do
-	#for i in 4 8 9 10 12 14 15; do
+	#for i in 4 8 9 10 12 14 15 16 17 18; do
 		timeout --kill-after 4100 3600 ./run.py -w 0 -q $i --pool-size $b --time 3000  $@
 		echo "Waiting a bit..."
 		sleep 30

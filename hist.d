ac1e9119082cced1007fb8d5dd00d03358359d57
Dietrich Daroch <dietr1ch@acm.org>
2016-09-08 15:25:26 -0300

    Add request-time histogram collector

---
+++ hist.d
@@ -0,0 +1,29 @@
+#!/usr/bin/env rdmd
+
+// Round floating point numbers
+import std.algorithm, std.conv, std.functional,
+    std.math, std.regex, std.stdio;
+
+int[] hist = new int[5001];
+
+int count(char[] line) {
+	double delay = to!double(line);
+	int d = cast(int)(round(delay*100));
+	if(d>=hist.length)
+		d=hist.length-1;
+
+	hist[d]++;
+	return 1;
+}
+
+void main() {
+	stdin.byLine.map!count;
+
+	foreach(line; stdin.byLine)
+		count(line);
+
+	int sum = 0;
+	foreach(int i, int c; hist)
+		if(c)
+			writef("%d, %d\n",i, sum+=c);
+}

84253af55dbbc112de5987c236e3e36ac7b58948
Dietrich Daroch <dietr1ch@acm.org>
2016-09-12 13:52:37 -0300

    Fix histogram resolution

--- hist.d
+++ hist.d
@@ -4,15 +4,16 @@
 import std.algorithm, std.conv, std.functional,
     std.math, std.regex, std.stdio;
 
-int[] hist = new int[5001];
+immutable auto resolutionInv = 1/0.01;  // 0.01s -> 100
+int[] hist = new int[1+10*resolutionInv];
 
 int count(char[] line) {
-	double delay = to!double(line);
-	int d = cast(int)(round(delay*100));
+	double delay = to!double(line); // 0.4299
+	int d = cast(int)(round(delay*resolutionInv)); // 42.999; 43
 	if(d>=hist.length)
 		d=hist.length-1;
 
-	hist[d]++;
+	hist[d]++;  // Vote for 0.43s  (index 43)
 	return 1;
 }
 
